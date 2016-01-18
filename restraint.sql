cd5e3ab1e17dd21073be4de06b21c7811984ca0c
jschaum <schaum@goldmail.etsu.edu>
2016-01-18 00:34:04 -0500

    Update restraint.sql

--- restraint.sql
+++ restraint.sql
@@ -1,1 +1,3 @@
+--Unsure if primary key is auto incremented or comes from another table. 236 is the value given to the ability in the ability.h file,
+--if that is the incorrect PK to use please let me know
 INSERT INTO `abilities` VALUES (236, 'retaliation', '1', '77', '1', '600', '1', '115', '0', '0', '20.0', '0', '1', '80', '0', '0',null);
