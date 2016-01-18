87dc5d68c2808c8e1509d23a201d1678e32b8de4
jschaum <schaum@goldmail.etsu.edu>
2016-01-18 00:16:44 -0500

    Create restraint.lua

---
+++ restraint.lua
@@ -0,0 +1,27 @@
+-----------------------------------
+-- Ability: Restraint
+-- Enhances your weapon skill power with each normal attack you land.
+-- Obtained: Warrior Level 77
+-- Recast Time: 10:00
+-- Duration: 5:00
+-----------------------------------
+
+
+require("scripts/globals/status");
+
+-----------------------------------
+-- onAbilityCheck
+-----------------------------------
+
+function onAbilityCheck(player,target,ability)
+    return 0,0;
+end;
+
+-----------------------------------
+-- onUseAbility
+-----------------------------------
+
+function onUseAbility(player,target,ability)
+    --increased duration slightly, 300 is standard
+    player:addStatusEffect(EFFECT_RESTRAINT,1,0,420);
+end;
